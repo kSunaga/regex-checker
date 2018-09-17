99f8342a38ff098536d8e751de0ded15bc597731
kSunaga <jokers4140@gmail.com>
2018-09-17 14:32:22 +0900

    構造を決定

--- main.rb
+++ main.rb
@@ -5,7 +5,16 @@ get "/" do
   erb :index
 end
 
-post "/send" do
-  @input_regix = params[:input_regix]
+post "/" do
+  input_regix = params[:input_regix]
+  object_text = params[:object_text]
+
+  @is_read = params[:read]
+
+  @result = object_text.match(input_regix)
+
+  p @result
+  p @is_read
+
   erb :index
 end
