48b0c8ded4a34961e06639accbdda72580dfcfb4
kSunaga <jokers4140@gmail.com>
2018-09-30 18:02:31 +0900

    置換を実装

--- main.rb
+++ main.rb
@@ -8,10 +8,27 @@ end
 post "/" do
   @input_regix = params[:input_regix]
   @object_text = params[:object_text]
+  @replased_text = params[:replased_name]
+  # option = params[:input_option]
 
   @is_read = params[:read]
 
-  @result = @object_text.match(@input_regix)
+  if params[:replased]
+    @result = @object_text.gsub(@input_regix, @replased_text)
+  else
+    @result = @object_text.match(@input_regix)
+  end
+
+  # 後にオプションを実装
+  # if params[:replased] && option
+  #   @result = @object_text.gsub(@input_regix, "/#{replased_text}/#{option}")
+  # elsif !option.empty?
+  #   regix = Regix.compile(/"#{@input_regix}/option)
+  #   @result = @object_text.match(regix)
+  #   p @result
+  # else
+  #   @result = @object_text.match(@input_regix)
+  # end
 
   erb :index
 end
